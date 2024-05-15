4c5e7d92a709cc04e57117065e24548c4c7ccd08
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-16 01:16:00 +0900

    feat: 장바구니에 상품 추가 및 삭제 api 호출 로직 구현

--- web/board-project/src/pages/Cart.vue
+++ web/board-project/src/pages/Cart.vue
@@ -1,22 +1,82 @@
 <template>
   <div class="cart">
-    카트 페이지
+    <div class="container">
+      <ul>
+        <li v-for="(i, idx) in state.items" :key="idx">
+          <img :src="i.imgPath" />
+          <span class="name">{{ i.name }}</span>
+          <span class="price">{{ lib.getNumberFormat(i.price - i.price * i.discountRate / 100) }}원</span>
+          <i class="fa fa-trash" @click="removeItem(i.id)"></i>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import { reactive } from 'vue';
 import axios from '../axios'
+import lib from '../scripts/lib'
 
 export default {
   setup() {
-    axios.get('/member/cart/item').then((res) => {
-      console.log(res.data);
+    const state = reactive({
+      items: []
     })
 
+    const loadItem = () => {
+      axios.get('/member/cart/item').then((res) => {
+        console.log(res.data);
+        state.items = res.data.result;
+      })
+    }
+
+    const removeItem = (itemId) => {
+      axios.delete(`/member/cart/item/${itemId}`).then(() => {
+        loadItem();
+      })
+    }
+
+    loadItem();
+
     return {
+      state,
+      lib,
+      loadItem,
+      removeItem,
     }
   }
 }
 </script>
 <style scoped>
+.cart ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.cart ul li {
+  border: 1px solid #eee;
+  margin-top: 25px;
+  margin-bottom: 25px;
+}
+
+.cart ul li img {
+  width: 150px;
+  height: 150px;
+}
+
+.cart ul li .name {
+  margin-left: 25px;
+}
+
+.cart ul li .price {
+  margin-left: 25px;
+}
+
+.cart ul li i {
+  float: right;
+  font-size: 20px;
+  margin-top: 65px;
+  margin-right: 50px;
+}
 </style>
