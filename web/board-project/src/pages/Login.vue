4a188a4c43b98fcebd36e733b740bffb16705b0e
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-18 23:15:05 +0900

    feat: 이메일 중복 확인 api 호출

--- web/board-project/src/pages/Login.vue
+++ web/board-project/src/pages/Login.vue
@@ -3,10 +3,15 @@
     <div class="form-container sign-up-container">
       <div class="sign-form">
         <h1>Create Account</h1>
-        <span>use your email for registration</span>
-        <input type="text" placeholder="Nickname" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <span>type your email for registration</span>
+        <div class="email-container">
+          <input type="email" class="email" placeholder="Email" v-model="registerState.form.email" />
+          <button class="duplicate-check" @click="emailCheck()">중복 확인</button>
+        </div>
+        <small v-if="registerState.message" :class="{'text-available': registerState.messageType === 'available', 'text-unusable': registerState.messageType === 'unusable'}">{{ registerState.message }}</small>
+        <input type="text" class="nickName" placeholder="Nickname" v-model="registerState.form.nickName" />
+        <input type="password" class="password" placeholder="Password" v-model="registerState.form.password" />
+        <input type="password" class="password-confirm" placeholder="Confirm your password"  v-model="registerState.form.passwordConfirm" />
         <button>Sign Up</button>
       </div>
     </div>
@@ -14,8 +19,8 @@
       <div class="sign-form">
         <h1>Sign in</h1>
         <span>use your account</span>
-        <input type="email" placeholder="Email" @keyup.enter="submit()" v-model="state.form.email" />
-        <input type="password" placeholder="Password" @keyup.enter="submit()" v-model="state.form.password" />
+        <input type="email" placeholder="Email" @keyup.enter="submit()" v-model="loginState.form.email" />
+        <input type="password" placeholder="Password" @keyup.enter="submit()" v-model="loginState.form.password" />
         <a href="#">Forgot your password?</a>
         <button @click="submit()">Sign In</button>
       </div>
@@ -29,7 +34,7 @@
         </div>
         <div class="overlay-panel overlay-right">
           <h1>Hello, Friend!</h1>
-          <p>Enter your personal details and use gallery</p>
+          <p>Enter your personal details and use our service</p>
           <button class="ghost" id="signUp" @click="activePanel(true)">Sign Up</button>
         </div>
       </div>
@@ -44,13 +49,6 @@ import store from '../scripts/store'
 
 export default {
   setup() {
-    const state = reactive({
-      form: {
-        email: "",
-        password: "",
-      }
-    })
-
     const isActive=  ref(false);
 
     /**
@@ -59,10 +57,53 @@ export default {
      */
     const activePanel = (status) => {
       isActive.value = status;
-    }
+    };
+
+    /**
+     * 회원가입을 위한 양식
+     */
+    const registerState = reactive({
+      form: {
+        email: "",
+        nickName: "",
+        password: "",
+        passwordConfirm: "",
+      },
+      message: "",
+      messageType: ""
+    });
+
+    /**
+     * 로그인을 위한 양식
+     */
+    const loginState = reactive({
+      form: {
+        email: "",
+        password: "",
+      }
+    });
+
+    /**
+     * 이메일 중복 체크
+     */
+    const emailCheck = () => {
+      axios.get(`/auth/check-email?email=${registerState.form.email}`).then((res) => {
+        console.log(res.data);
+        registerState.message = res.data.message;
+        registerState.messageType = 'available';
+      }).catch((error) => {
+        if(error.response && error.response.data) {
+          registerState.message = error.response.data.message;
+          registerState.messageType = 'unusable';
+        } else {
+          registerState.message = '사용할 수 없는 이메일입니다.';
+          registerState.messageType = 'unusable';
+        }
+      });
+    };
 
     const submit = () => {
-      axios.post('/auth/login', state.form).then((res) => {
+      axios.post('/auth/login', loginState.form).then((res) => {
         const accessToken = res.headers["authorization"];
 
         if(accessToken) {
@@ -79,9 +120,11 @@ export default {
     }
 
     return {
-      state,
+      registerState,
+      loginState,
       isActive,
       activePanel,
+      emailCheck,
       submit
     }
   }
