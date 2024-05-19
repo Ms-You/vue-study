aa9181950c87b7424ae7a7dd331db5bfb07bb04d
Myeongsu Yu <msu122005@hs.ac.kr>
2024-05-19 14:07:58 +0900

    feat: 상품 검색 로직 추가

--- api/src/main/java/com/board/api/service/ItemService.java
+++ api/src/main/java/com/board/api/service/ItemService.java
@@ -16,11 +16,38 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
 
+    /**
+     * 상품 목록 조회
+     * @return
+     */
     @Transactional(readOnly = true)
     public List<ItemDTO.ItemResp> getItemList() {
         List<ItemDTO.ItemResp> itemRespList = new ArrayList<>();
 
         itemRepository.findAll()
+                .stream()
+                .map(item -> itemRespList.add(ItemDTO.ItemResp.builder()
+                        .id(item.getId())
+                        .name(item.getName())
+                        .imgPath(item.getImgPath())
+                        .price(item.getPrice())
+                        .discountRate(item.getDiscountRate())
+                        .build()))
+                .collect(Collectors.toList());
+
+        return itemRespList;
+    }
+
+    /**
+     * 상품 검색
+     * @param name
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public List<ItemDTO.ItemResp> searchItemList(String name) {
+        List<ItemDTO.ItemResp> itemRespList = new ArrayList<>();
+
+        itemRepository.findByNameContains(name)
                 .stream()
                 .map(item -> itemRespList.add(ItemDTO.ItemResp.builder()
                         .id(item.getId())
